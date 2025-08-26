b35e7087721b9adb7de5f05af60fd2e9620ac5a7
IMRAN WAHEED <imran.waheed@edu.unife.it>
2025-08-26 21:26:02 +0200

    colab

---
+++ frontend/src/app/componenti/colab/colab.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-colab',
+  imports: [],
+  templateUrl: './colab.html',
+  styleUrl: './colab.css'
+})
+export class Colab {
+
+}
--- frontend/src/app/pagine/home/home.ts
+++ frontend/src/app/pagine/home/home.ts
@@ -3,11 +3,12 @@ import { Header } from '../../componenti/header/header';
 import { Search } from "../../componenti/search/search";
 import { Categoria } from "../../componenti/categoria/categoria";
 import { Footer } from '../../componenti/footer/footer';
+import {Colab} from "../../componenti/colab/colab";
 
 
 @Component({
   selector: 'app-home',
-  imports: [Header, Search, Categoria, Footer],
+  imports: [Header, Search, Categoria, Footer, Colab],
   templateUrl: './home.html',
   styleUrls: ['./home.css']
 })
