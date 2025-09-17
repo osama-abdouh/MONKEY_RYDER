f74aa8b531effa4cea8cfafc3b488e0bcbd472c9
oss <osama.abdouh@edu.unife.it>
2025-09-17 13:04:16 +0200

    Allineo i contenuti di main a os

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,18 +1,4 @@
 import { Routes } from '@angular/router';
-<<<<<<< HEAD
-import { Home } from './pagine/home/home';
-import { Cerca } from './pagine/cerca/cerca';
-import { Contattaci } from './pagine/contattaci/contattaci';
-import { SplashPage } from './pagine/splashPage/splashPage';
-import {Policy} from "./pagine/policy/policy";
-
-export const routes: Routes = [
-  { path: '', component: SplashPage },
-  { path: 'home', component: Home },
-  { path: 'cerca', component: Cerca },
-  { path: 'contattaci', component: Contattaci },
-  { path: 'policy', component: Policy }
-=======
 import { HomeComponent } from './features/home/home';
 import { CartComponent } from './features/cart/cart';
 import { RegisterComponent } from './features/auth/register/register';
@@ -29,5 +15,4 @@ export const routes: Routes = [
     { path: 'wishlist', component: WishlistComponent },
     { path: 'contact-us', component: ContactUsComponent },
     { path: '**', redirectTo: '' } // Rotta di fallback per URL non validi
->>>>>>> 025a782116520e57b9576adfa264847de08dead9
 ];
