6843c2b994cf251ee190174a83c278dd552d23b5
oss <osama.abdouh@edu.unife.it>
2025-09-23 17:23:26 +0200

    supabase DB collegato

--- backend/db/init.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE utenti (
-    id SERIAL PRIMARY KEY,
-    nome VARCHAR(100),
-    email VARCHAR(100)
-);
-
-INSERT INTO utenti (nome, email) VALUES
-('Mario Rossi', 'mario.rossi@example.com'),
-('Luca Bianchi', 'luca.bianchi@example.com');
